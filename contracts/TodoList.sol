54cb3d690939fa7612911c8aa92a363024302fbe
ankitboghra <ankitboghra@gmail.com>
2021-04-10 17:36:27 +0530

    new: events and toggle function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,9 @@ contract TodoList {
 
     mapping(uint256 => Task) public tasks;
 
+    event TaskCreated(uint256 id, string content, bool completed);
+    event TaskToggled(uint256 id, bool completed);
+
     constructor() public {
         createTask("Start learning blockchain");
     }
@@ -18,5 +21,13 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskToggled(_id, _task.completed);
     }
 }
